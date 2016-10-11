146e2825ddedfc89125b88b42ffd517e618826ad
cantwait <cadenas.rafael@gmail.com>
2016-10-10 22:42:32 -0500

    #3 auction refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AuctionsComponent } from './auction/auctions.component';
+import { AuctionComponent } from './auction/auction.component';
 import {AuctionService} from './auction/auction.service';
 import { Deferred } from './utils/util.deferred';
 import {routing} from './app.routing';
@@ -12,12 +12,13 @@ import {UserService} from './user/user.service';
 import {NavbarComponent} from './navbar/navbar.component';
 import {HomeComponent} from './home/home.component';
 import {ProfileComponent} from './profile/profile.component';
+import {AutionListComponent} from './auction/auction-list.component';
 
 
 @NgModule({
   imports: [BrowserModule, routing],
   providers: [BidService, ParseWrapper, AuctionService, UserService],
-  declarations: [AppComponent, AuctionsComponent, NavbarComponent,HomeComponent, ProfileComponent],
+  declarations: [AppComponent, AuctionComponent, NavbarComponent,HomeComponent, ProfileComponent,AutionListComponent],
   bootstrap: [AppComponent]
 })
 
---
+++ src/app/auction/auction-list.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import { AuctionService } from './auction.service'
+
+@Component({
+    selector: "auction-list",
+    templateUrl: "./auction-list.component.html",
+})
+export class AutionListComponent {
+
+    private auctions: any[];
+
+    constructor(private auctionService: AuctionService){
+        this.auctionService.getAuctions()
+            .then((auctions) => this.auctions = auctions)
+            .catch((error) => console.error(error))
+    }
+}

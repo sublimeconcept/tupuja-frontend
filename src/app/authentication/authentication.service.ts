d2e8d2159fb24e55165372ce4df22e78e2cc8ae4
Rafael Cadenas <cadenas.rafael@gmail.com>
2016-11-01 00:59:23 -0500

    login hotfix

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Router }       from '@angular/router';
-import { UserService } from '../user/user.service';
-import { AlertService } from '../alert/alert.service';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import { Injectable } from              '@angular/core';
+import { Router }       from            '@angular/router';
+import { UserService } from             '../user/user.service';
+import { Observable } from              'rxjs/Observable';
+import                                  'rxjs/add/operator/map';
+import { FlashMessagesService } from    'angular2-flash-messages';
 
 @Injectable()
 export class AuthenticationService {
     constructor(
-        private router: Router, private userService: UserService, private alertService: AlertService) { }
+        private router: Router, private userService: UserService, private _flashMessagesService: FlashMessagesService) { }
  
     login(username, password) {
         this.userService.logIn(username, password).then(
             (user) => {
-                this.alertService.success('Welcome.', true);
+                this._flashMessagesService.show('Bienvenido.', {cssClass: 'alert-success',timeout:3000});
                 this.router.navigate(['/']);
             })
             .catch((error) => {
-                this.alertService.error(error.message);
-                console.error(JSON.stringify(error))
+                this._flashMessagesService.show(error.message,{cssClass: 'alert-danger',timeout:3000});
+                console.error(JSON.stringify(error));
             });
     }
  
